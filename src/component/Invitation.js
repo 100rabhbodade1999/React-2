dc71b973e7c881f85b241900bdbeb66d23826139
100rabhbodade1999 <srbhbdd99@gmail.com>
2023-04-21 14:12:31 +0530

    Invitation Ass.

---
+++ src/component/Invitation.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import { Fragment } from "react";
+
+export function MailContent (props){
+
+    return(
+        <Fragment>
+        <p>Subject: {Subject}</p>
+        <p>To : {SentMail}</p>
+        <p>Hi, {Name}</p>
+
+        </Fragment>
+    )
+}
